af09dac3af8765ac2449664273f776c7d9d0b14f
Kazu Komoto <kazuhiro.komoto@students.makeschool.com>
2015-11-25 15:20:28 -0800

    Initialized

---
+++ OpenCVFaceDetectionSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenCVFaceDetectionSample
+//
+//  Created by 小林和宏 on 11/25/15.
+//  Copyright © 2015 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
